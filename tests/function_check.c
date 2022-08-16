5ba997ff6dd27699675e8e417ae9eb20097e8b53
ayamorcito <izquierdoz.g@gmail.com>
2022-08-16 10:09:57 -0700

    added function check

---
+++ tests/function_check.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * func_cheq -  checks opcode and returns the correct function
+ * @str: the opcode
+ *
+ * Return: returns a function, or NULL on failure
+ */
+
+instruct_func func_cheq(char *str)
+{
+	int i;
+
+	instruction_t instruct[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{NULL, NULL},
+	};
+
+	i = 0;
+	while (instruct[i].f != NULL) 
+	{
+		if (str != NULL && strcmp(instruct[i].opcode, str) == 0)
+			return (instruct[i].f);
+		i++;
+	}
+
+	return (instruct[i].f);
+}
