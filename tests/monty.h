fea9b837af75fcf0e4762ac2ce7d2d47292ee83c
ayamorcito <izquierdoz.g@gmail.com>
2022-08-16 10:01:48 -0700

    added structures in main

---
+++ tests/monty.h
@@ -0,0 +1,48 @@
+#ifndef _MONTY
+#define _MONTY
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
+/**
+ * FUNCTION PROTOTIPES
+ */
+void opfile(char *filename, stack_t **stack);
+instruct_func func_cheq(char *str);
+void _push(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
+
+#endif 
