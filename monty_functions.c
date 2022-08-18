c3b4588743f3597935fb612b3bb84d411c025e97
ayamorcito <izquierdoz.g@gmail.com>
2022-08-18 12:24:43 -0700

    betty style fix

---
+++ monty_functions.c
@@ -0,0 +1,75 @@
+#include "monty.h"
+/**
+ * _push - push int to a stack
+ * @stack: linked lists for monty stack
+ * @line_number: number of linei opcode occurs on
+ */
+
+void _push(stack_t **stack, unsigned int line_number)
+{
+        char *arg;
+        int push_arg = 0;
+        int i = 0;
+
+        arg = strtok(NULL, "\n\t\r ");
+
+        while (arg[i])
+        {
+                if (!arg[i])
+                {
+                        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                        exit(EXIT_FAILURE);
+                }
+
+                else if (arg[i] >= 48 && arg[i] <= 57)
+                {
+                        push_arg = atoi(arg);
+                        add_dnodeint(stack, push_arg);
+                        i++;
+                }
+
+        }
+/*if (stack)
+        {
+                arg = strtok(NULL, "\n\t\r");
+                while (arg[i])
+                {
+                if (!arg[i])
+                {
+                        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                        exit(EXIT_FAILURE);
+                }
+                else if (isdigit(arg[i]) != 0)
+                {
+                        push_arg = atoi(arg);
+                        add_dnodeint(stack, push_arg);
+                        i++;
+                }
+                else
+                {
+                        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                        exit(EXIT_FAILURE);
+                }
+                }
+        }
+        */
+}
+
+/**
+ * _pall - print all function
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ */
+void _pall(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
+{
+        stack_t *print_all;
+
+        print_all = *stack;
+        while (print_all)
+        {
+                printf("%d\n", print_all->n);
+                print_all = print_all->next;
+        }
+
+}
+
