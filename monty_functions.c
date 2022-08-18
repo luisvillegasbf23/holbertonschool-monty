cf947fb666336201ce999cae1df0277d096233a7
ayamorcito <izquierdoz.g@gmail.com>
2022-08-18 12:46:39 -0700

    betty style fix

--- monty_functions.c
+++ monty_functions.c
@@ -7,54 +7,27 @@
 
 void _push(stack_t **stack, unsigned int line_number)
 {
-        char *arg;
-        int push_arg = 0;
-        int i = 0;
+	char *arg;
+	int push_arg = 0;
+	int i = 0;
 
-        arg = strtok(NULL, "\n\t\r ");
+	arg = strtok(NULL, "\n\t\r ");
 
-        while (arg[i])
-        {
-                if (!arg[i])
-                {
-                        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                        exit(EXIT_FAILURE);
-                }
-
-                else if (arg[i] >= 48 && arg[i] <= 57)
-                {
-                        push_arg = atoi(arg);
-                        add_dnodeint(stack, push_arg);
-                        i++;
-                }
-
-        }
-/*if (stack)
-        {
-                arg = strtok(NULL, "\n\t\r");
-                while (arg[i])
-                {
-                if (!arg[i])
-                {
-                        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                        exit(EXIT_FAILURE);
-                }
-                else if (isdigit(arg[i]) != 0)
-                {
-                        push_arg = atoi(arg);
-                        add_dnodeint(stack, push_arg);
-                        i++;
-                }
-                else
-                {
-                        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                        exit(EXIT_FAILURE);
-                }
-                }
-        }
-        */
+	while (arg[i])
+	{
+		if (!arg[i])
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+		else if (arg[i] >= 48 && arg[i] <= 57)
+		{
+			push_arg = atoi(arg);
+			add_dnodeint(stack, push_arg);
+			i++;
+		}
+	}
 }
-
 /**
  * _pall - print all function
  * @stack: pointer to linked list stack
@@ -62,14 +35,40 @@ void _push(stack_t **stack, unsigned int line_number)
  */
 void _pall(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
 {
-        stack_t *print_all;
+	stack_t *print_all;
 
-        print_all = *stack;
-        while (print_all)
-        {
-                printf("%d\n", print_all->n);
-                print_all = print_all->next;
-        }
+	print_all = *stack;
 
+	while (print_all)
+	{
+		printf("%d\n", print_all->n);
+		print_all = print_all->next;
+	}
 }
 
+/*
+ *
+ * if (stack)
+ * {
+	arg = strtok(NULL, "\n\t\r");
+	while (arg[i])
+	{
+		if (!arg[i])
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+		else if (isdigit(arg[i]) != 0)
+		{
+			push_arg = atoi(arg);
+			add_dnodeint(stack, push_arg);
+			i++;
+		}
+		else
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+*/
