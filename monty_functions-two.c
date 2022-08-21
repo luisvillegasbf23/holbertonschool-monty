f1ded6b42ff091be84835ca282d774f7b4a4c138
luisville.lv@gmail.com <luisville.lv@gmail.com>
2022-08-20 17:48:43 -0700

    nop function fix

--- monty_functions-two.c
+++ monty_functions-two.c
@@ -1,10 +1,11 @@
+#include "monty.h"
 /**
  * nop - does nothing
  * @stack: pointer to stack
  * @line_number: line number of instruction
  * Return: void
  */
-void _nop(stack_t **stack, unsigned int line_number)
+void _not(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;
