b32abd36100b96615e294767dda0d8cfdc2d7fc2
luisville.lv@gmail.com <luisville.lv@gmail.com>
2022-08-20 17:37:10 -0700

    nop function

---
+++ monty_functions-two.c
@@ -0,0 +1,11 @@
+/**
+ * nop - does nothing
+ * @stack: pointer to stack
+ * @line_number: line number of instruction
+ * Return: void
+ */
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
